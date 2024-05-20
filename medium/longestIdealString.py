8a0bb27f02ff6bd30642c4d1ed537c8dd69e2749
Joel Eckles <joeleckles@microsoft.com>
2024-05-20 16:01:40 -0400

    adding max tree depth

--- medium/longestIdealString.py
+++ medium/longestIdealString.py
@@ -2,8 +2,22 @@
 
 class Solution:
     def longestIdealString(self, s: str, k: int) -> int:
-        s = s.split()
-        i, j, longest = 0, 1, -1
-        while i < (len(s) - 1):
-            while j < len(s):
-                if ord()
+        s, substrings = list(s), list(s[0])
+        for i in range(1, len(s)):
+            updated, toAdd = False, []
+            for index, substring in enumerate(substrings):
+                if abs(ord(s[i]) - ord(substring[-1])) <= k: # if this condition is met, add s to the end of each substring   
+                    toAdd.append(substrings[index] + str(s[i]))  
+                    updated = True
+            substrings += toAdd
+            if not(updated):                    
+                substrings.append(s[i]) # is this condition is not met, add s as a new entry into the list of subStrings
+        maxLen = -1
+        for substring in substrings:
+            if len(substring) > maxLen:
+                maxLen = len(substring)    
+        return maxLen
+    
+sol, s, k = Solution(), "dyyonfvzsretqxucmavxegvlnnjubqnwrhwikmnnrpzdovjxqdsatwzpdjdsneyqvtvorlwbkb", 7
+
+print(sol.longestIdealString(s, k))
